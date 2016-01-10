d6678bc7c421f70edfe010cd68e619dde5655692
AurelieFak <aurelf13@hotmail.fr>
2016-01-10 03:58:01 +0100

    Ch1

--- makepdf.sh
+++ makepdf.sh
@@ -25,15 +25,18 @@ mkdir -p ${outputDir}
 dateNow="date +%m-%d__%H:%M"
 outputName="${name}_$(eval $dateNow)"
 
-latexInBuildDir="-output-directory=${outputDir} -aux-directory=${outputDir}"
-latexcommand="pdflatex -output-directory=${outputDir} -aux-directory=${outputDir} -interaction=nonstopmode"
+latexInBuildDir="-output-directory=${outputDir}"
+# -aux-directory=${outputDir}"
+latexcommand="pdflatex -output-directory=${outputDir} -interaction=nonstopmode --enable-write18"
+# -aux-directory=${outputDir} -interaction=nonstopmode"
 mycmd="${latexcommand} $name"
 
 ##############################
 ## EXECUTE COMMANDS
 ##############################
 $mycmd
-cp library.bib ${outputDir}
+\rm ./${outputDir}/library.bib
+\cp ./library.bib ${outputDir}
 (cd ${outputDir}; bibtex $name;);
 (cd ${outputDir}; makeglossaries $name;);
 $mycmd
