a9ce6c23aebb7d0eb10d6ddb56bb320d7e7718e5
Joris Schafers <imjoris@gmail.com>
2016-01-31 03:44:36 +0100

    Fixed layout and structure issues

---
+++ 5-patterns/splitUpTex.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# fileToSPlit='patterns.tex'
+fileToSPlit='oldpatterns.tex'
+newSplitFile='new-'"$fileToSPlit";
+
+prevLine='';
+prevFName='';
+prevSec=''
+
+grep -nr 'section{' "$fileToSPlit" | 
+
+while read -r i; do
+    lineNum=$(echo "$i" | cut -d : -f 1);
+    secLineLimit="$(( lineNum-1 ))";
+    
+    secName="${i#*\{}";
+    secName="${secName%\}*}";
+    
+    #replace illegal chars
+
+    # echo "section: $secName";
+
+    
+    if [[ ! -z $prevLine ]]; then
+        echo '';
+        echo '##############################'
+        echo "$prevLine"'-'"$secLineLimit";
+        fName=$(echo "$prevSec" | sed -e 's/[^A-Za-z0-9._-]/_/g')'.tex';
+        sed -n "$prevLine"','"$secLineLimit"'p' "$fileToSPlit" > "$fName";
+        echo '\input{5-patterns/'"$fName"'}' >> "$newSplitFile";
+
+    else
+        sed -n ','"$secLineLimit"'p' "$fileToSplit" > "$newSplitFile";
+    fi
+
+    prevSec="$secName";
+    prevLine="$lineNum";
+done
+        # fName=$(echo "$prevSec" | sed -e 's/[^A-Za-z0-9._-]/_/g')'.tex';
+        # echo "$prevLine";
+        # echo "$prevSec";
+
+        # sed -n "$prevLine"',$p' "$fileToSPlit" > "$fName";
+        # echo '\input{'"$fName"'}' >> "$newSplitFile";
+
+
