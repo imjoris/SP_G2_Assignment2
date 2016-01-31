0ce7ec3a1c5fcc4e589aa7f38f2426f4f65a4815
Joris Schafers <imjoris@gmail.com>
2016-01-31 21:42:35 +0100

    layer and active rep enhance merge temp

--- makepdf.sh
+++ makepdf.sh
@@ -2,8 +2,25 @@
 
 file='report';
 
-#rubber -d $target
-pdflatex "$file" > /dev/null
-pdflatex "$file" > /dev/null
-makeglossaries "${file%.*}" > /dev/null
-rubber -d "$target";
+# rubber -d $target
+ pdflatex "$file" > /dev/null
+ pdflatex "$file" > /dev/null
+ makeglossaries "${file%.*}" > /dev/null
+ rubber -d "$file";
+
+# backupDir='/home/joris/RugP22/SoftwarePatterns/pdfBackup/'
+backupDir='../pdfBackup'
+mkdir -p "$backupDir";
+
+backupDir=$(cd "$backupDir"; pwd);
+
+# echo $backupDir;
+
+now=$(date +"%H-%M__%m-%d-%Y");
+newFName="$bacupDir"'/report-'"$now"'.pdf';
+# # mkdir -p "$backupDir";
+
+cp "$file"'.pdf' "${backupDir}";
+(cd "${backupDir}"; mv "${file}"'.pdf' "report-$now"'.pdf';)
+
+
